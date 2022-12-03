9df2a06105cd06c64408e0cb54fbefbc49ae1f73
kryst.schr <kryst.schr@gmail.com>
2022-12-04 00:57:29 +0100

    Day 3 done

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -4,6 +4,7 @@
     {
         public static void Main(string[] args)
         {
+            //PART 1
             int sum = 0;
             string[] input = File.ReadAllLines(@"../../../input.txt");
             foreach (string line in input) 
@@ -24,7 +25,29 @@
                     }
                 }
             }
-            Console.WriteLine(sum);
+            Console.WriteLine($"Part 1 answear: {sum}");
+            //PART 2
+            sum = 0;
+            for (int i = 0; i < input.Length; i += 3)
+            {
+                char[] firstElfsSack = input[i].ToCharArray();
+                char[] secondElfsSack = input[i+1].ToCharArray();
+                char[] thirdElfsSack = input[i+2].ToCharArray();
+                char[] commonItem = firstElfsSack.Intersect(secondElfsSack).ToArray();
+                commonItem = commonItem.Intersect(thirdElfsSack).ToArray();
+                foreach (var item in commonItem)
+                {
+                    if (Char.IsLower(item))
+                    {
+                        sum = sum + (int)item - 96;
+                    }
+                    else if (Char.IsUpper(item))
+                    {
+                        sum = sum + (int)item - 38;
+                    }
+                }
+            }
+            Console.WriteLine($"Part 2 answear: {sum}");
         }
     }
 }
