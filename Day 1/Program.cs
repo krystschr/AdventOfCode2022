de7357596656e66b09180cff09d2c5b76dd81aec
kryst <kryst@BESTIA>
2022-12-03 23:58:22 +0100

    Dodaj pliki projektów.

---
+++ Day 1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Day_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Elf> elfs = new List<Elf>();
+            string[] input = File.ReadAllLines(@"../../../input.txt");
+            int summary = 0;
+            int maxCalories = 0;
+            foreach(string line in input)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    elfs.Add(new Elf { CaloriesSummary = summary });
+                    summary = 0;
+                }
+                else
+                {
+                    summary = summary + int.Parse(line);
+                }
+            }
+            foreach(Elf elf in elfs)
+            {
+                if(elf.CaloriesSummary > maxCalories)
+                    maxCalories= elf.CaloriesSummary;
+            }
+            Console.WriteLine(maxCalories);
+            Console.ReadLine();
+        }
+    }
+
+    class Elf
+    {
+        public int CaloriesSummary { get; set; }
+    }
+}
---
+++ Day 3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Day_3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            string[] input = File.ReadAllLines(@"../../../input.txt");
+            foreach (string line in input) 
+            {     
+                char[] rucksack = line.ToCharArray();
+                var firstCompartment = rucksack.Take(rucksack.Length / 2).ToArray();
+                var secondCompartment = rucksack.Skip(rucksack.Length / 2).ToArray();
+                char[] commonItem = firstCompartment.Intersect(secondCompartment).ToArray();
+                foreach(var item in commonItem)
+                {
+                    if (Char.IsLower(item))
+                    {
+                        sum = sum + (int)item - 96;
+                    }
+                    else if (Char.IsUpper(item))
+                    {
+                        sum = sum + (int)item - 38;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
