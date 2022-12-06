785634476f15633ad9385fcff265f78eeb2ba0b1
kryst.schr <kryst.schr@gmail.com>
2022-12-06 23:32:00 +0100

    day 6

---
+++ Day_6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day_5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"../../../input.txt");
+            List<char> chars = input[0].ToCharArray().ToList();
+            bool itWorked = false;
+            for(int i = 0; i < input[0].Length; i++)
+            {
+                List<char> fourLetters = chars.Skip(i).Take(4).ToList();
+                List<char> fourteenLetters = chars.Skip(i).Take(14).ToList();
+
+                if (fourLetters.Count == fourLetters.Distinct().Count() && !itWorked)
+                {
+                    itWorked= true;
+                    Console.WriteLine(i + 4);
+                }
+                if (fourteenLetters.Count == fourteenLetters.Distinct().Count())
+                {
+                    Console.WriteLine(i + 14);
+                }
+            }
+        }
+    }
+}
